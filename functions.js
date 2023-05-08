cddd98f47adc0ec343274bb52ffe9dd1c12af17b
Lean014 <livanhees@gmail.com>
2023-05-08 18:10:54 +0200

    from step 2 tp setp 3

--- functions.js
+++ functions.js
@@ -1,22 +1,22 @@
 function makeConnection(onReceive) {
     console.log("trying to connect!");
     OOCSI.connect("wss://oocsi.id.tue.nl/ws","phoneprotoguide"); 
-    OOCSI.subscribe("LPtest", onReceive);
+    OOCSI.subscribe(nameOfChannel , onReceive);
 }
 
 function makeConnectionIndu(onReceive) {
     let userName = sessionStorage.getItem("userKey"); // no const for when is null give a name
-    userName ??= nameGenerator(); //?? means null. is no user name go to namd genarator and give name. 
+    userName ??= nameGenerator(); //?? means null. is no user name go to namd genarator and give name. !!! comment this line out at demoDay
 
     console.log("trying to connect!");
     console.log(userName);
 
     OOCSI.connect("wss://oocsi.id.tue.nl/ws",userName); 
-    OOCSI.subscribe("LPtest", onReceive);
+    OOCSI.subscribe(nameOfChannel, onReceive);
 }
 
 function nameGenerator() {
-    const randomNumb = Math.random()*1000; //bteween 0 and 0 so times 1000. let is variable. const is constant
+    const randomNumb = Math.random()*10000; //bteween 0 and 0 so times 10000. let is variable. const is constant
     const randomNumbRound = Math.floor(randomNumb); //make it a round number. 
     const userName = "indu"+randomNumbRound;
     console.log(userName);// print in JS concole
